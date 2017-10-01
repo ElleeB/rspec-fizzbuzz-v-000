233ac5f9c78ec35d4a8031dec9992d97d6594482
Laura Trager <laura.trager@gmail.com>
2017-10-01 21:34:30 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,12 @@
 def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
-  elseif int % 5 == 0
+  elseif int % 5 == 0 
     "Buzz"
-  else int % 15 == 0
+  else int % 3 == 0 && int % 5 == 0
     "FizzBuzz"
   end
 end
-
 
 #elseif int % 5 == 0
   #puts '"Buzz"'
